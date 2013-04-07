be1b58f7f29735541d249c1d9ef7ed3c2a599a86
RafaelCosman <rafaelCosman@gmail.com>
2013-04-07 07:19:44 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,17 @@
-var buttons = [ "Conway's Original", "Coral", "Amoeba", "Slow Burn", "Slow Burn 2", "Diamonds" ];
+var buttonNames = [ "Conway's Original", "Coral", "Amoeba", "Slow Burn", "Slow Burn 2", "Diamonds" ];
 
 $(document).ready(function() {
     appendButtons();
 });
 
 var appendButtons = function() {
+    console.log(buttons.length);
     for (var example = 0; example < buttons.length; example++) {
-        $('.example-rules').append('<button class="example-button" id="btn' + example + '">' + getButton(example) + '</button>');
+        $('.example-rules').append('<button class="example-button" id="btn' + example + '">' + buttonNames[example] + '</button>');
     }
     $(".example-rules button").click(function() {
         console.log($(this).index());
 
-        GoL3D.setRules(getButton($(this).index()));
+        GoL3D.setRules(buttonNames[($(this).index())]);
     });
 };
-
-
-var getButton = function(button) {
-    return buttons[button];
-}
