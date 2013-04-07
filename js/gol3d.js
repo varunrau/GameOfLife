8c978c60f7fea2b736267a3c4f309e7872aff8a3
varunrau <varunrau@gmail.com>
2013-04-07 07:31:09 -0700

    random

--- js/gol3d.js
+++ js/gol3d.js
@@ -107,8 +107,10 @@ var GoL3D = {
     if(this.liveCubes != undefined){
         for (var x = 0; x < this.size; x++)
             for(var y = 0; y < this.size; y++) {
+                console.log(this.liveCubes)
                 cube = this.liveCubes[x][y];
                 if(cube) {
+                    console.log(cube.age);
                     cube.age++;
                     var hue = (.3*Math.sqrt(cube.age)) % 1.0;
                     cube.material.color.setHSV(hue,1.0,1.0);
