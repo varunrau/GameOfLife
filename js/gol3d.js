1c845408c895b84bbf5ca33804d8b7ce80dc7052
RafaelCosman <rafaelCosman@gmail.com>
2013-04-07 08:02:33 -0700

    Update gol3d.js

--- js/gol3d.js
+++ js/gol3d.js
@@ -112,7 +112,7 @@ var GoL3D = {
                     cube.age++;
                     var hue = (.1*Math.sqrt(cube.age)) % 1.0;
                     cube.material.color.setHSV(hue,1.0,1.0);
-                    cube.position.z = age;
+                    cube.position.z = 10-age;
                 }
             }
         }
